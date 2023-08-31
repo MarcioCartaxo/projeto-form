ad2ebcf43c4bcf46e0803345147dbf289dedbc94
João Victor <131309292+JoaoVictorMachadoSantos@users.noreply.github.com>
2023-08-31 14:58:09 -0300

    update-js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-const form = document.querySelector("#form");
+let nome;
+
+
+
+// Verifica se a página atual é a página inicial (index.html)
+if (window.location.pathname.includes("index.html")) {
+    document.getElementById("button").addEventListener("click" , function(){
+         nome = document.getElementById("nome").value;
+        localStorage.setItem("nome", nome);
+        window.location.href = "obrigado.html";
+    });
+}
+
+// Verifica se a página atual é a página "obrigado.html"
+if (window.location.pathname.includes("obrigado.html")) {
+    const nome = localStorage.getItem("nome");
+    if (nome) {
+        document.getElementById("home").innerHTML = "Obrigado " + nome + " seu feedback já foi enviados.";
+    }
+}
